6fb9cc7f9f069b9b5cb3a3707b1e5c402c4024e9
Balasubramaniam M <131952962+b1238155@users.noreply.github.com>
2023-09-28 15:36:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Define a functional React component
+function HelloWorld() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>React Learning</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React!</h1>
     </div>
   );
 }
 
-export default App;
+export default HelloWorld;
